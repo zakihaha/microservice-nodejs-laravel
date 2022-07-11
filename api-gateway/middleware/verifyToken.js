ef2817cb1b2e3e82c7ad46edd3e9ca57b262dbde
Muh Zaki Choiruddin <zakichoiruddin24@gmail.com>
2022-07-11 23:56:13 +0700

    middleware token

---
+++ api-gateway/middleware/verifyToken.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = process.env;
+
+module.exports = async (req, res, next) => {
+    const token = req.headers.authorization;
+    
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                status: 'error',
+                message: err.message
+            })
+        }
+
+        req.user = decoded;
+        return next();
+    });
+}
