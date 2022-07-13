e920d0fb60a852f2cc26352f84c22c0bd121ced6
Muh Zaki Choiruddin <zakichoiruddin24@gmail.com>
2022-07-13 23:15:30 +0700

    create migrations and models

--- service-course/database/migrations/2022_07_13_153241_create_my_courses_table.php
+++ service-course/database/migrations/2022_07_13_153241_create_my_courses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class CreateMyCoursesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('my_courses', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
+            $table->integer('user_id');
+            $table->unique(['course_id', 'user_id']);
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('my_courses');
     }
 }
