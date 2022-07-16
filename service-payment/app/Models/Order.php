6b37dc7ed68a2a12ffc0aa7bffca586c3ddbe404
Muh Zaki Choiruddin <zakichoiruddin24@gmail.com>
2022-07-17 00:09:58 +0700

    setup model

--- service-payment/app/Models/Order.php
+++ service-payment/app/Models/Order.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+
+    protected $fillable = [
+        'user_id',
+        'course_id',
+        'status',
+        'snap_url',
+        'metadata',
+    ];
 }
--- service-payment/app/Models/PaymentLog.php
+++ service-payment/app/Models/PaymentLog.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class PaymentLog extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+        'metadata' => 'array',
+    ];
+
+    protected $fillable = [
+        'order_id',
+        'status',
+        'payment_type',
+        'raw_response',
+    ];
 }
